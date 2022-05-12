a84f0ebbb34912fd9f8d46d5e78969dfd6bb1fc8
Vinicius Miranda <vini.s.miranda@hotmail.com>
2022-05-11 22:58:56 -0300

    navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navbar from './components/navbar';
 import logo from './logo.svg';
+
 
 function App() {
   return (
-    <h1>Página Principal</h1>
+    <Navbar />
   );
 }
 
